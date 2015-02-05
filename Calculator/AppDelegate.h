f7cde0cb7530e25691afa0ad3067471df5e91e86
KASEY MOFFAT <kaseymoffat@KASEYs-MacBook-Pro.local>
2015-02-05 11:57:38 -0800

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by KASEY MOFFAT on 2/5/15.
+//  Copyright (c) 2015 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by KASEY MOFFAT on 2/5/15.
+//  Copyright (c) 2015 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
